ada85342a1b2cc43f2302100a00e8692f69cd94f
carlsondultra <carlsondultra@users.noreply.github.com>
2025-02-22 21:21:41 -0500

    fetching station names every 10 seconds

---
+++ src/app/bikefetch/fetch.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+interface Station {
+  station_id: string;
+  name: string;
+  // Add other properties as needed
+}
+
+interface ClientComponentProps {
+  initialData: Station[];
+}
+
+const getApi = async (): Promise<Station[]> => {
+  const response = await fetch(process.env.NEXT_PUBLIC_BIKE_API);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const newData = await response.json();
+  return newData.data.stations;
+};
+
+export default function ClientComponent({ initialData }: ClientComponentProps) {
+  const [data, setData] = useState<Station[]>(initialData);
+
+  const fetchData = async () => {
+    try {
+      const newData = await getApi();
+      setData(newData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+
+    const interval = setInterval(fetchData, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from API</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.station_id}>{item.name}</li>
+        ))}
+        {console.log(data[0].name)}
+        {console.log(data[1].name)}
+      </ul>
+    </div>
+  );
+}
